0ecb6b47322685bf67cfaf5ac0de0b995e13f68a
Brad Schrag <brad.schrag@gmail.com>
2019-01-17 17:49:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,8 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! I BUILT THIS!"
-    "Shotgun FTW!!!"
+    "Welcome to your app!!!! I BUILT THIS!\nShotgun FTW!!!"
   end
 
 end
